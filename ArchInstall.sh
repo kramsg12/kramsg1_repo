8288a788a80b1124746b7c125f80d84bdd08fe5e
kramsg12 <33532860+kramsg12@users.noreply.github.com>
2020-09-08 12:16:22 +1000

    Update ArchInstall.sh

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -94,7 +94,6 @@ arch-chroot /mnt chsh -s /usr/bin/zsh
 arch-chroot /mnt systemctl enable gdm.service
 arch-chroot /mnt systemctl enable NetworkManager.service
 arch-chroot /mnt pacman -S --noconfirm kramsg1-cinnimon 
-arch-chroot /mnt gpasswd -a input "$user" network
 echo "$user:$password" | chpasswd --root /mnt
 echo "root:$password" | chpasswd --root /mnt
 echo ' '
