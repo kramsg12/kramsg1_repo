b2d3b88bfcdcfe297cee51c27213aa847d1b124e
kramsg12 <33532860+kramsg12@users.noreply.github.com>
2020-09-13 17:04:39 +1000

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -2,4 +2,4 @@
 $user = Sys.info()[["user"]]
 wget https://github.com/kramsg12/kramsg1_repo/raw/master/desktop/cinnamon.tar.xz
 tar -xf cinnamon.tar.xz
-cp -rf ./cinnamon/skel/* /home/$user/
+cp -rf ./cinnamon/skel/* $HOME
