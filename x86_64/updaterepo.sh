fa6e547dc979826a7f7eade8a1ccc7c2c913356c
kramsg12 <kramsg19981405@gmail.com>
2020-09-13 11:17:41 +1000

    Update

---
+++ x86_64/updaterepo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm kramsg1_repo*
+
+
+echo "repo-add"
+repo-add kramsg1_repoo.db.tar.gz *.pkg.tar.xz
+repo-add kramsg1_repo.db.tar.gz *.pkg.tar.zst
+#sleep 8
+cp -f kramsg1_repo.db.tar.gz kramsg1_repo.db
+
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################"
