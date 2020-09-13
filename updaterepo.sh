012f08289bacfcaf07c508826c64dcced87f5b9a
kramsg12 <33532860+kramsg12@users.noreply.github.com>
2020-09-13 11:16:08 +1000

    Update updaterepo.sh

--- updaterepo.sh
+++ updaterepo.sh
@@ -4,8 +4,8 @@ rm kramsg1_repo*
 
 
 echo "repo-add"
-repo-add -s -n -R kramsg1_repoo.db.tar.gz *.pkg.tar.xz
-repo-add -s -n -R kramsg1_repo.db.tar.gz *.pkg.tar.zst
+repo-add kramsg1_repoo.db.tar.gz *.pkg.tar.xz
+repo-add kramsg1_repo.db.tar.gz *.pkg.tar.zst
 #sleep 8
 cp -f kramsg1_repo.db.tar.gz kramsg1_repo.db
 
