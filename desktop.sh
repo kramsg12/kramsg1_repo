bc4812b99ad8b700609fb9e17eab819e740df972
kramsg12 <33532860+kramsg12@users.noreply.github.com>
2020-09-13 17:48:50 +1000

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,1 +1,1 @@
-sudo wget -c https://github.com/kramsg12/kramsg1_repo/raw/master/desktop/cinnamon.tar.xz -O - | sudo tar -xz -C $HOME
+sudo wget -c https://raw.githubusercontent.com/kramsg12/kramsg1_repo/master/desktop/cinnamon.tar.gz -O - | sudo tar -xz -C $HOME
